89e564e6b029f7d02a06086c1586d136cac13b14
Guilherme <guilhermebarroso166@gmail.com>
2026-05-12 17:44:56 -0300

    configuração inicial do supabase

---
+++ src/lib/supabase/client.tsx
@@ -0,0 +1,17 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("Variável de ambiente NEXT_PUBLIC_SUPABASE_URL não definida.");
+}
+if (!supabaseKey) {
+  throw new Error("Variável de ambiente NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY não definida.");
+}
+
+export const createClient = () =>
+  createBrowserClient(
+    supabaseUrl,
+    supabaseKey,
+  );
