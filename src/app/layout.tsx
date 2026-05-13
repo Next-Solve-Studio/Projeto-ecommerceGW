de60d25414adb29b8b89b0c2237c76e3c951e110
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-05-12 22:03:36 -0300

    Subindo a estrutura para criação do Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import Header from "@/layout/header/Header";
+
+
 export const metadata: Metadata = {
     title: "E-Commerce",
     description: "Loja de Games",
@@ -13,6 +16,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="pt-BR" className={`h-full antialiased`}>
+            <Header />
             <body className="min-h-full flex flex-col">{children}</body>
         </html>
     );
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,23 @@
-
+import Logo from "./sections/Logo"
+import SearchBar from "./sections/SearchBar"
+import Buttons from "./sections/Buttons"
+import Department from "./sections/Department"
+import Navigation from "./sections/Navigation"
 
 export default function Header() {
     return (
-        <header className="">
-            
+        <header className="w-full fixed h-20 bg-black">
+            <section className="">
+                <div className="">
+                    <Logo />
+                    <SearchBar />
+                    <Buttons />
+                </div>
+                <div className="">
+                    <Department />
+                    <Navigation />
+                </div>
+            </section>
         </header>
     )
 }
